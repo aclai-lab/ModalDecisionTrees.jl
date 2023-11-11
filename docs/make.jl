6ce6e298377d1177ca74e7d29e48b80a26e70fb3
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-11-11 16:32:39 +1100

    Fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -21,8 +21,7 @@ makedocs(;
 
 deploydocs(;
     repo = "github.com/aclai-lab/ModalDecisionTrees.jl",
-    devbranch = "main",
     target = "build",
     branch = "gh-pages",
-    versions = ["stable" => "v^", "v#.#"],
+    versions = ["main" => "main", "stable" => "v^", "v#.#", "dev" => "dev"],
 )
