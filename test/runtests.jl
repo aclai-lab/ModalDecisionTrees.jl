ffd8460d1fb32e33edcde61a159746ecaf7cccb0
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-02-24 19:34:13 +0100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,7 @@ using StatsBase: mean
 
 using ModalDecisionTrees: build_stump, build_tree, build_forest
 
-Pkg.update() # For MLDatasets
+# Pkg.update() # For MLDatasets
 
 println("Julia version: ", VERSION)
 
